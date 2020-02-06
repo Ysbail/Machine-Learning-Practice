e82722ab1a9ce66946af51ece7ea1f413712567c
Ysbail <rudowlosa@gmail.com>
2020-02-06 21:44:13 +0100

    add grid search

---
+++ Python/Model Selection/grid_search.R
@@ -0,0 +1,49 @@
+dataset = read.csv('Social_Network_Ads.csv')
+dataset = dataset[3:5]
+
+
+dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
+
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
+
+
+library(e1071)
+classifier = svm(formula = Purchased ~ .,
+                 data = training_set,
+                 type = 'C-classification',
+                 kernel = 'radial')
+
+
+y_pred = predict(classifier, newdata = test_set[-3])
+cm = table(test_set[, 3], y_pred)
+
+library(caret)
+folds = createFolds(training_set$Purchased, k = 10)
+cv = lapply(folds, function(x){
+  training_fold = training_set[-x, ]
+  test_fold = training_set[x, ]
+  classifier = svm(formula = Purchased ~ .,
+                   data = training_fold,
+                   type = 'C-classification',
+                   kernel = 'radial')
+  y_pred = predict(classifier, newdata = test_fold[-3])
+  cm = table(test_fold[, 3], y_pred)
+  accuracy = (cm[1,1] + cm[2,2]) / (cm[1,1] + cm[2,2] + cm[1,2] + cm[2,1])
+  return(accuracy)
+})
+
+
+acc = mean(as.numeric(cv))
+
+library(caret)
+classifier = train(form = Purchased ~ ., data = training_set, method = 'svmRadial')
+classifier
+classifier$bestTune
+
---
+++ Python/Model Selection/grid_search.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+classifier = SVC(kernel='rbf')
+classifier.fit(X_train, y_train)
+
+
+y_pred = classifier.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+acc = accuracy_score(y_test, y_pred)
+
+# k-fold
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+accuracies.mean()
+accuracies.std()
+
+# grid search
+parameters = [{'C': [1, 10, 100, 1000], 'kernel': ['linear']}, 
+              {'C': [1, 10, 100, 1000], 'kernel': ['rbf'], 'gamma': [0.13, 0.12, 0.11, 0.1, 0.09, 0.08, 0.07, 0.06]}, 
+              ]
+
+grid_search = GridSearchCV(estimator = classifier, 
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10)
+grid_search = grid_search.fit(X_train, y_train)
+best_accuracy = grid_search.best_score_
+best_parameters = grid_search.best_params_
